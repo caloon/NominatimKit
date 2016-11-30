7dd694cb2bada2d3ab73b14ceecfefea269138e0
deveo <tools@josefmoser.de>
2016-11-30 19:32:52 +0100

    Project Setup

---
+++ NominationSwift/NominationSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NominationSwift
+//
+//  Created by Josef Moser on 30/11/2016.
+//  Copyright © 2016 Josef. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
