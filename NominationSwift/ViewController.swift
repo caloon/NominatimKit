9413be9632069f9a4407ab94309a8065694bd1e7
deveo <tools@josefmoser.de>
2016-11-30 22:42:04 +0100

    Location methods implemented; public API modified (async)

--- NominationSwift/ViewController.swift
+++ NominationSwift/ViewController.swift
@@ -12,17 +12,19 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
     
-    let location = Nominatim.getLocation(fromString: "22644 Lund")
-    print(location.countryCode)
+    // get location details through free text search
+    Nominatim.getLocation(fromAddress: "The Royal Palace of Stockholm", completion: {(error, location) -> Void in
+      print("Geolocation of the Royal Palace of Stockholm:")
+      print("lat = " + (location?.latitude)! + "   lon = " + (location?.longitude)!)
+    })
+    
+    // get location details through latitude & longitude search
+    Nominatim.getLocation(fromLatitude: "55.6867243", longitude: "12.5700724", completion: {(error, location) -> Void in
+      print("---")
+      print("City for geolocation 55.6867243/12.5700724:")
+      print(location?.city)
+    })
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-
 }
 
